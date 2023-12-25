691f598ed9a70db5aef1bf02cf3f1bfd8b3f9855
Carloslt <carloslt.proyectos@gmail.com>
2023-12-25 20:20:22 +0100

    delete services pgadmin

--- app/src/services/post.services.ts
+++ app/src/services/post.services.ts
@@ -1,11 +1,13 @@
 import axios, { AxiosInstance } from 'axios'
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5005/api'
+
 class PostServices {
   instance: AxiosInstance
 
   constructor() {
     this.instance = axios.create({
-      baseURL: import.meta.env.VITE_API_URL,
+      baseURL: API_URL,
     })
   }
 
